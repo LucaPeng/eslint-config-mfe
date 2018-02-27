ff511060a99485a0d4dbdab03395402ae81a0661
宋鹏 <songpeng02@meituan.com>
2018-02-27 17:32:02 +0800

    modify ts-node rules

--- eslintrc.typescript-node.js
+++ eslintrc.typescript-node.js
@@ -5,6 +5,8 @@ module.exports = {
     'typescript'
   ],
   rules: {
+    // ts will trans code to specify ecma version, no need to check unsupported-features
+    'node/no-unsupported-features': 'off',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/first': 'off'
