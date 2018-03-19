2f613a6531aa2b914547ebdadfdd3ba7bd07368b
宋鹏 <songpeng02@meituan.com>
2018-03-19 15:18:25 +0800

    upgrade node rules

--- eslintrc.node.js
+++ eslintrc.node.js
@@ -17,5 +17,11 @@ module.exports = {
     'no-process-env': 'off',
     // 考虑到在正常逻辑中会使用到，关闭该规则
     'no-process-exit': 'off',
+    // 在 node 中使用 console 在 terminal 中进行输出是一种正常的编码方式
+    'no-console': 'off',
+    // 对参数属性的修改在处理可变数据时是必要的
+    'no-param-reassign': ['error', { 'props': false }],
+    // 限制过于严格 
+    'prefer-promise-reject-errors': 'off',
   }
 };
--- eslintrc.typescript-node.js
+++ eslintrc.typescript-node.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     // ts will trans code to specify ecma version, no need to check unsupported-features
     'node/no-unsupported-features': 'off',
+    // 基于构建框架会对资源索引进行一些模式化的设计，关闭关于模块引入的一些规则
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/first': 'off'
